708e3967e0db25bcc1e71607f83c39b988ab51f8
wesbasinger <wbasinger@villagetechschools.org>
2016-12-11 14:47:32 -0600

    I think the logic for the game is set.

---
+++ game/game.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+import random
+
+class Game(object):
+    
+    def __init__(self):
+        
+        f = open("script.txt")
+        raw = f.read()
+        f.close()
+        delim = raw.split(".")
+        
+        self.lines = delim
+        self.streak = 0
+        
+        g = open("red_herrings.txt")
+        raw = g.read()
+        raw = raw.split("\n")
+            
+        self.red_herrings = raw
+        
+    
+    def get_next_line(self):
+        
+        return self.lines.pop(0).replace("\n", " ")
+        
+    
+    def scramble(self):
+        
+        next_line = self.get_next_line()
+        
+        delim = next_line.split(" ")
+        
+        candidate = None
+        
+        while not candidate:
+            
+            possible_word = random.choice(delim)
+            if len(possible_word) > 1:
+                candidate = possible_word
+        
+        choices_seed = random.sample(self.red_herrings, 2)
+        choices_seed.append(candidate)
+        random.shuffle(choices_seed)
+        
+        #return (next_line.replace(candidate, "---"), candidate)
+        return {
+                'display': next_line.replace(candidate, "---"),
+                'missing_word': candidate,
+                'choices': choices_seed
+        }
+        
+    def verify(self, scrambled_dict, ix):
+        if scrambled_dict["choices"][ix] == scrambled_dict["missing_word"]:
+            self.streak += 1
+            return True
+        else:
+            self.streak = 0
+            return False
+            
+    def get_streak(self):
+        return self.streak
+        
